e81e5afb5278f999219c3fce82e8a964351a6b97
Wipeshot <domikel02@outlook.de>
2022-05-13 14:44:59 +0200

    Design: PlaceField

--- Project/src/client/gui/PlaceField.java
+++ Project/src/client/gui/PlaceField.java
@@ -1,9 +1,16 @@
 package client.gui;
 
+import client.Client;
 import client.gui.components.GameLabel;
+import client.gui.components.ShipPlaceLabel;
+import server.field.Ship;
 
+import javax.sound.sampled.Line;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PlaceField {
 
@@ -12,14 +19,83 @@ public class PlaceField {
     private JPanel gamePanel = new JPanel(new GridLayout(10,10));
     private GameLabel[] gameComponents = new GameLabel[100];
 
-    public PlaceField(Container container) {
-        contentPane = container;
-        contentPane.removeAll();
-        setup();
+    private JPanel shipPanel = new JPanel(new GridLayout(5,1));
+    private ShipPlaceLabel[] shipPlaceLabels = new ShipPlaceLabel[5];
+
+    private JLabel readyButton = new JLabel("Ready", SwingConstants.CENTER);
+    private JLabel reset = new JLabel("Reset", SwingConstants.CENTER);
+    private JLabel joinCode = new JLabel("", SwingConstants.CENTER);
+
+    private Ship[] ships = new Ship[5];
+
+    public PlaceField(Container contentPane, Client client) {
+        this.contentPane = contentPane;
+        setupPlaceField();
+        setupShipField();
+        setupButtons();
     }
 
-    private void setup() {
+    private void setupPlaceField() {
+        gamePanel.setBounds(50, 50, 550, 550);
+        gamePanel.setBorder(new LineBorder(Color.BLACK));
+        gamePanel.setBackground(Color.BLACK);
+        gamePanel.setOpaque(true);
+        contentPane.add(gamePanel);
+        addGameComponentsToField();
+    }
 
+    private void addGameComponentsToField() {
+        for(int i = 0; i < gameComponents.length; i++) {
+            gameComponents[i] = new GameLabel(i);
+            gameComponents[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+
+                }
+            });
+            gamePanel.add(gameComponents[i]);
+        }
+    }
+
+    private void setupShipField() {
+        shipPanel.setBounds(740, 50, 200, 250);
+        shipPanel.setBorder(new LineBorder(Color.BLACK));
+        shipPanel.setBackground(Color.GRAY);
+        shipPanel.setOpaque(true);
+        contentPane.add(shipPanel);
+        addShipsToShipField();
+    }
+
+    private void addShipsToShipField() {
+        shipPlaceLabels[0] = new ShipPlaceLabel("Schlachtschiff", 5);
+        shipPlaceLabels[1] = new ShipPlaceLabel("Kreuzer", 4);
+        shipPlaceLabels[2] = new ShipPlaceLabel("Fregatte", 3);
+        shipPlaceLabels[3] = new ShipPlaceLabel("Fregatte", 3);
+        shipPlaceLabels[4] = new ShipPlaceLabel("Minensucher", 2);
+        for(ShipPlaceLabel lbl : shipPlaceLabels) {
+            shipPanel.add(lbl);
+        }
+    }
+
+    private void setupButtons() {
+        readyButton.setBounds(740, 550, 200, 50);
+        readyButton.setBorder(new LineBorder(Color.BLACK));
+        reset.setBounds(740, 500, 200, 50);
+        reset.setBorder(new LineBorder(Color.BLACK));
+        joinCode.setBounds(740, 470, 200, 30);
+        contentPane.add(readyButton);
+        contentPane.add(reset);
+        contentPane.add(joinCode);
     }
 
     public Container getContentPane() {
@@ -33,7 +109,8 @@ class MainPlaceField {
         JFrame jf = new JFrame();
         jf.setVisible(true);
         jf.setSize(1024,720);
-        PlaceField placefield = new PlaceField(jf.getContentPane());
+        jf.setLayout(null);
+        PlaceField placefield = new PlaceField(jf.getContentPane(), new Client());
         jf.setContentPane(placefield.getContentPane());
     }
 }
--- Project/src/client/gui/components/ShipPlaceLabel.java
+++ Project/src/client/gui/components/ShipPlaceLabel.java
@@ -7,17 +7,21 @@ import java.awt.*;
 public class ShipPlaceLabel extends JLabel {
 
     private String name;
-    private int id;
+    private int length;
 
-    public ShipPlaceLabel(String name, int id) {
+    public ShipPlaceLabel(String name, int length) {
         this.name = name;
         this.setText(name);
-        this.id = id;
+        this.length = length;
         this.setOpaque(true);
         this.setBackground(Color.lightGray);
         this.setBorder(new LineBorder(Color.BLACK));
         this.setHorizontalAlignment(SwingConstants.CENTER);
         this.setVerticalAlignment(SwingConstants.CENTER);
+    }
+
+    public int getLength() {
+        return length;
     }
 
 }
